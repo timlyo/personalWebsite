5013a4ac05f8e955b76b49b04e20f7c711ba4ec4
tim <timlyomaddison@gmail.com>
2015-08-20 21:30:15 +0100

    work on threading tutorials

--- setupPageList.py
+++ setupPageList.py
@@ -7,20 +7,23 @@ def getFileTags(fileName):
 		text = file.read()
 		tags = []
 		for meta in re.findall("<meta.*name='keywords'.*>", text):
-			for result in re.findall("content='[a-z, ]*'", meta):
+			for result in re.findall("content='[a-zA-Z, ]*'", meta):
 				for tag in result.replace("content=", "").replace("'", "").replace(" ", "").split(","):
 					tags.append(tag)
 
 		return tags
 
-
+# execution point
 files = glob.glob("dist/*.html")
 
 with open("dist/javascript/pageList.js", "w") as output:
 	output.write("var pages = {};\n")
+	output.write("//Tag = Page\n")
 	for file in files:
 		key = file.replace(".html", "").replace("dist/", "")
-		output.write("pages['" + key + "'] = '" + file.replace("dist/", "") + "';\n")
 		tags = getFileTags(file)
+		if "noSearch" in tags:
+			continue
+		output.write("pages['" + key + "'] = '" + file.replace("dist/", "") + "';\n")
 		for tag in tags:
 			output.write("pages['" + tag + "'] = '" + file.replace("dist/", "") + "';\n")
